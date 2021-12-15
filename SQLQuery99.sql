47536025505cee43c0bd34dcbdff9f0b6712a7c6
0lia <95088213+0lia@users.noreply.github.com>
2021-12-15 14:06:41 +0200

    Add files via upload

---
+++ SQLQuery99.sql
@@ -0,0 +1,21 @@
+insert into Person (age, health) values (24, 2)
+insert into Person (age, health) values (42, 5)
+insert into Person (age, health) values (87, 4)
+insert into Person (age, health) values (76, 1)
+insert into Person (age, health) values (98, 2)
+insert into Person (age, health) values (23, 5)
+
+insert into Car ([type], motor_volume, car_price) values ('car', 1600, 56000)
+insert into Car ([type], motor_volume, car_price) values ('car', 1900, 69000)
+insert into Car ([type], motor_volume, car_price) values ('bus', 5600, 560000)
+insert into Car ([type], motor_volume, car_price) values ('truck', 4600, 122000)
+insert into Car ([type], motor_volume, car_price) values ('motorcycle', 600, 13000)
+insert into Car ([type], motor_volume, car_price) values ('bus', 7600, 748000)
+
+
+insert into House (area, house_price) values (98, 116000)
+insert into House (area, house_price) values (200, 999000)
+insert into House (area, house_price) values (50, 46000)
+insert into House (area, house_price) values (76, 110500)
+insert into House (area, house_price) values (500, 9999990)
+insert into House (area, house_price) values (150, 502070)
